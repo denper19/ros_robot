9bdff4280dba313ec5e1fc2da2279ab9cf484433
jermito24 <jermito24@gmail.com>
2026-05-24 03:51:36 +0000

    okay robot behaves the same as gazebo in rviz

--- goomba_bot/launch/launch_sim.launch.py
+++ goomba_bot/launch/launch_sim.launch.py
@@ -9,31 +9,39 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
+
+
 def generate_launch_description():
 
-    package_name = 'goomba_bot'
 
-    rsp_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
-        )
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='goomba_bot' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-        )
-    )
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
 
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'goomba_bot'],
-        output='screen'
-    )
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
 
+
+
+    # Launch them all!
     return LaunchDescription([
-        rsp_launch,
-        gazebo_launch,
-        spawn_entity
+        rsp,
+        gazebo,
+        spawn_entity,
     ])
--- goomba_bot/launch/rsp.launch.py
+++ goomba_bot/launch/rsp.launch.py
@@ -23,12 +23,14 @@ def generate_launch_description():
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{
+            'robot_description': robot_description_config.toxml(),
+            'use_sim_time': use_sim_time,
+        }],
     )
 
     # Launch!

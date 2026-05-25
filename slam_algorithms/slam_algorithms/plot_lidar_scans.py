f0238df06194472c6a3bca9cbba35ed23c4919e7
jermito24 <jermito24@gmail.com>
2026-05-25 04:59:53 +0000

    updated it to make it a viz node that looks at odom and laser scan

--- slam_algorithms/slam_algorithms/plot_lidar_scans.py
+++ slam_algorithms/slam_algorithms/plot_lidar_scans.py
@@ -1,13 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 import matplotlib.pyplot as plt
 
-class LaserscanNode(Node):
-
+class DataVisualizerNode(Node):
+    """
+    Plots the lidar data as the robot moves. 
+    """
     def __init__(self):
         
-        super().__init__('laser_scan_plotter')
+        super().__init__('data_visualizer')
         
         self.get_logger().info('Laser scan plotter node initialized')
         
@@ -17,6 +20,13 @@ class LaserscanNode(Node):
             self.laser_scan_callback,
             10
         )
+
+        self.odom_subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10
+        )
     
     def laser_scan_callback(self, msg):
 
@@ -24,13 +34,19 @@ class LaserscanNode(Node):
         
         self.get_logger().info(f'{len(msg.ranges)} ranges')
         
-        return msg  
+        return msg
+
+    def odom_callback(self, msg):
+
+        self.get_logger().info(f'Received odom data')
+        
+        return msg
 
 def main(args=None):
 
     rclpy.init(args=args)
 
-    subscriber = LaserscanNode()
+    subscriber = DataVisualizerNode()
 
     rclpy.spin(subscriber)
 
